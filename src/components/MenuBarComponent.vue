2f4bb3287765deb4de3faf4c751db6d132b4950a
gabriel.bezerra <gabriel18lb@outlook.com>
2024-04-06 13:21:59 -0300

    fix styles navbar

--- src/components/MenuBarComponent.vue
+++ src/components/MenuBarComponent.vue
@@ -1,9 +1,23 @@
 <template>
-    <v-app-bar app dark elevation="2">
-      <v-toolbar-title style="color: #4CAF50; font-weight: bold;">HELP.me</v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn plain color="green" to="/unidades">Unidades</v-btn>
-      <v-btn plain color="green" to="/empresas">Empresas</v-btn>
-      <v-btn plain color="green" to="/leiloes">Leilões</v-btn>
-    </v-app-bar>
+  <v-app-bar app dark elevation="2" class="navbar">
+    <v-toolbar-title class="logo">HELP.me</v-toolbar-title>
+    <v-divider vertical class="mx-4"></v-divider>
+    <v-spacer></v-spacer>
+    <v-btn plain color="green" to="/unidades">Unidades</v-btn>
+    <v-btn plain color="green" to="/empresas">Empresas</v-btn>
+    <v-btn plain color="green" class="p-0" to="/leiloes">Leilões</v-btn>
+  </v-app-bar>
 </template>
+
+<style>
+.logo {
+  color: #4CAF50;
+  font-weight: bold;
+  padding-right: 16px;
+}
+
+.navbar {
+  padding-right: 2rem;
+  padding-left: 2rem;
+}
+</style>
